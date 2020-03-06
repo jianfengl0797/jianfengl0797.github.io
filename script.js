f5c19214a3075557a81910b3fd8963836395085b
JianFeng Li <jianfengl0797@hstat.org>
2020-03-06 21:49:59 +0000

    index.html stuff

--- script.js
+++
@@ -1,9 +0,0 @@
-let ownerName = "JianFeng Li"
-let userName = "jianfengl0797";
-
-document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
-});
-
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
